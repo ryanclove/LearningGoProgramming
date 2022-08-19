04fe9f9372b9814d3f24a3c20be6a6adc60c9e57
Ryan Coslove <ryancoslove@gmail.com>
2022-08-19 13:53:01 -0400

    Updates to Lecture>Section 6.45

--- Lecture Sections/Section 6 Programming Fundamentals/Sec 6.45/main.go
+++ Lecture Sections/Section 6 Programming Fundamentals/Sec 6.45/main.go
@@ -1,7 +1,52 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	// GOOS and GOARCH Packages
+	"runtime"
+)
+
+// Numeric types
+
+// can specify types, but not necessary for execution here for x and y
+
+var x int
+var y float64
+
+// if I want exact precision in bits (for memory/storage purposes)
+// signed 8-bit goes from -128 to 127
+var z int8 = -128
 
 func main() {
-	fmt.Println("")
+	x := 42
+	y := 42.34534
+	fmt.Println(x)
+	fmt.Println(y)
+	fmt.Printf("%T\n", x)
+	// type int
+	fmt.Printf("%T\n", y)
+	// type float64
+	//x = 42.55
+	// throws an error because now I've declared
+	// the type above as int, and now x is not an int
+	//fmt.Printf("%T\n", x)
+	int68()
+	runtimePackage()
+}
+
+func int68() {
+	fmt.Println("--------------")
+	fmt.Println(z)
+	fmt.Printf("%T\n", z)
+}
+
+func runtimePackage() {
+	fmt.Println("--------------")
+	fmt.Println(runtime.GOOS)
+	// prints OS running the program
+	// for me, outputs 'windows'
+	fmt.Println(runtime.GOARCH)
+	// runs the computer architecture running the program
+	// for me, outputs 'amd64' processor (64 bit)
+	// if was amd64p32, for example, would mean all pointers are 32-bit
 }
